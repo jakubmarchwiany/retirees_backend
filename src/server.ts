aaac6e72815c13b80b5cba30a3f419eb4ab6c9bd
xkvbikx <xkvbikx@gmail.com>
2022-10-10 15:59:27 +0200

    change url "/api" to "/backend"

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ class Server {
 
   private initializeControllers(controllers: Controller[]) {
     controllers.forEach((controller) => {
-      this.app.use("/api" + controller.path, controller.router);
+      this.app.use("/backend" + controller.path, controller.router);
     });
     this.app.use("*", (req: Request, res: Response, next: NextFunction) => {
       next(new HttpException(404, "Not found"));
