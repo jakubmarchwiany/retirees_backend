8d6085c726080912ed3d10090d8004c0430e096b
xkvbikx <xkvbikx@gmail.com>
2022-10-30 19:11:08 +0100

    change login logic

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -3,77 +3,67 @@ import { Request, Response, Router } from "express";
 import jwt from "jsonwebtoken";
 import Controller from "../interfaces/controller-interface";
 import authMiddleware from "../middleware/auth-middleware";
-import HttpException from "../middleware/exceptions/http-exception";
 import WrongCredentialsException from "../middleware/exceptions/wrong-credentials-exception";
 import TokenData from "../models/token-data-interface";
 import { USERS } from "../tmp/users";
 import catchError from "../utils/catch-error";
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, TOKEN_EXPIRE_AFTER } = process.env;
 
 class AuthenticationController implements Controller {
-  public router = Router();
-  public path = "/auth";
+    public router = Router();
+    public path = "/auth";
 
-  constructor() {
-    this.initializeRoutes();
-  }
-
-  private initializeRoutes() {
-    this.router.post(`/login`, catchError(this.loggingIn));
-    this.router.get(`/auto-login`, authMiddleware, catchError(this.autoLogin));
-    this.router.post(`/logout`, this.logOut);
-  }
-
-  private loggingIn = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-
-    for (const element of USERS) {
-      if (element.username == username) {
-        if (element.password == password) {
-          const tokenData = this.createAuthenticationToken();
-          // res.setHeader("Set-Cookie", [this.createCookie(tokenData)]);
-          res.cookie("Authorization", tokenData.token, {
-            maxAge: tokenData.expiresIn * 1000,
-            path: "/",
-          });
-          res.send({
-            message: "Udało się zalogować",
-          });
-          return;
-        } else {
-          throw new WrongCredentialsException();
-        }
-      }
+    constructor() {
+        this.initializeRoutes();
     }
 
-    throw new HttpException(
-      400,
-      `Konto nie istnieje. O takiej nazwie użytkownika`
-    );
-  };
+    private initializeRoutes() {
+        this.router.post(`/login`, catchError(this.loggingIn));
+        this.router.get(`/auto-login`, authMiddleware, catchError(this.autoLogin));
+        this.router.post(`/logout`, this.logOut);
+    }
 
-  private autoLogin = async (req: Request, res: Response) => {
-    res.send({
-      message: "Autoryzacja przebiegła pomyślnie",
-    });
-  };
+    private loggingIn = async (req: Request, res: Response) => {
+        const { username, password } = req.body;
 
-  private createAuthenticationToken(): TokenData {
-        const expiresIn = parseInt(TOKEN_EXPIRE_AFTER);
-    return {
-      expiresIn,
-      token: jwt.sign({}, JWT_SECRET, { expiresIn }),
+        for (const element of USERS) {
+            if (element.username == username) {
+                if (element.password == password) {
+                    const tokenData = this.createAuthenticationToken();
+                    res.cookie("Authorization", tokenData.token, {
+                        maxAge: tokenData.expiresIn * 1000,
+                        path: "/",
+                    });
+                    res.send({
+                        message: "Udało się zalogować",
+                    });
+                    return;
+                } else {
+                    throw new WrongCredentialsException();
+                }
+            }
+        }
+        throw new WrongCredentialsException();
     };
-  }
 
-  // private createCookie(tokenData: TokenData) {
-  //   return `Authorization=${tokenData.token}; Max-Age=${tokenData.expiresIn}; path=/;`;
-  // }
+    private autoLogin = async (req: Request, res: Response) => {
+        res.send({
+            message: "Autoryzacja przebiegła pomyślnie",
+        });
+    };
 
-  private logOut = (req: Request, res: Response) => {
-    res.setHeader("Set-Cookie", ["Authorization=; Max-Age=0; path=/;"]);
-    res.send({ message: "Udało się wylogować" });
-  };
+    private createAuthenticationToken(): TokenData {
+        const expiresIn = parseInt(TOKEN_EXPIRE_AFTER);
+        return {
+            expiresIn,
+            token: jwt.sign({}, JWT_SECRET, { expiresIn }),
+        };
+    }
+
+    private logOut = (req: Request, res: Response) => {
+        res.setHeader("Set-Cookie", ["Authorization=; Max-Age=0; path=/;"]);
+        res.send({ message: "Udało się wylogować" });
+    };
 }
 export default AuthenticationController;
